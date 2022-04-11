4227c7c2927534e47d437773c0d52857e29ed840
Angelika <101745895+avagava@users.noreply.github.com>
2022-04-11 19:04:45 +0300

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,8 @@ public class Main {
 
         Task2 task2 = new Task2();
         task2.calculate();
+
+        Task3 task3 = new Task3();
+        task3.calculate();
     }
 }
--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -3,14 +3,18 @@ package com.company;
 import java.util.Scanner;
 
 public class Task3 {
-    public void task3() {
+    public void calculate() {
         //Task 3
         System.out.println("Welcome to Task 3 program");
         Scanner scanner = new Scanner(System.in);
         Scanner intScanner = new Scanner(System.in);
         System.out.println("Please enter a number: ");
-        int p, i;
-        p = intScanner.nextInt();
+        int power = scanner.nextInt();
+        int start = 1;
+        do {
+            System.out.println(start);
+            start = start * 2;
+        } while (start < power);
 
     }
 }
